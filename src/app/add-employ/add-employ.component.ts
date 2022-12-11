c58c84b28fad62b80124f92b595a8780ed6c6183
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-11 21:28:41 +0530

    api task employ

--- src/app/add-employ/add-employ.component.ts
+++ src/app/add-employ/add-employ.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-employ',
@@ -14,11 +15,32 @@ company=""
 mobile=""
 username=""
 password=""
-
+constructor(private api:ApiService){}
 readvalue=()=>
 {
   let data:any={"empcode":this.empcode,"empname":this.empname,"designation":this.designation,"salary":this.salary,"company":this.company,
   "mobile":this.mobile,"username":this.username,"password":this.password}
   console.log(data)
+  this.api.addEmploy(data).subscribe(
+    (response:any)=>
+    {
+      console.log(response)
+      if(response.status=="success")
+      {
+        alert("employ added successfully")
+        this.company=""
+        this.designation=""
+        this.empcode=""
+        this.empname=""
+        this.mobile=""
+        this.password=""
+        this.salary=""
+        this.username=""
+      }
+      else{
+        alert("something went wrong")
+      }
+    }
+  )
 }
 }
--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-task',
@@ -9,9 +10,25 @@ export class AddTaskComponent {
 empcode=""
 task=""
 status=""
+constructor(private api:ApiService){}
 readValue=()=>
 {
   let data:any={"empcode":this.empcode,"task":this.task,"status":this.status}
   console.log(data)
+  this.api.addTask(data).subscribe(
+    (response:any)=>
+    {
+      console.log(response)
+      if(response.status=="success")
+      {
+        alert("task added")
+        this.empcode=""
+        this.task=""
+      }
+      else{
+        alert("something went wrong")
+      }
+    }
+  )
 }
 }
